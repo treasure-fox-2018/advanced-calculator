f5f5162840c01beb5a9a0baceafeb505e6d552e8
mario <panggihmario@gmail.com>
2018-06-04 17:46:52 +0700

    calculator

--- main.js
+++ main.js
@@ -3,3 +3,6 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+let cal = new Calculator(4)
+console.log(cal.add(7).subtract(2))
+// console.log(cal.squareRoot())
