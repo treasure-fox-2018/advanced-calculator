9363f80333d387c17931a890d6c2815b899e3ad5
fadhilhaka <fadhilhanri@gmail.com>
2018-06-04 14:26:52 +0700

    advanced calculator fadhil haka

--- calculator.js
+++ calculator.js
@@ -2,19 +2,27 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num1, num2) {
+    this.num1 = num1,
+    this.num2 = num2
   }
   add () {
+    return this.num1 + this.num2
   }
   subtract () {
+    return this.num1 - this.num2
   }
   multiply () {
+    return this.num1 * this.num2
   }
   divide () {
+    return this.num1 / this.num2
   }
   square () {
+    return Math.pow(this.num1, this.num2)
   }
   squareRoot () {
+    return Math.sqrt(this.num1)
   }
 }
 
