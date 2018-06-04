b1a5ab416386f1acd8404f781a631be5d229ac2a
dawimuhammad <muhamadhaddawi@gmail.com>
2018-06-04 15:11:54 +0700

    Haddawi - Calculator - Done

--- calculator.js
+++ calculator.js
@@ -1,21 +1,57 @@
 'use strict'
 
 class Calculator {
-  //write your code here
-  constructor () {
-  }
-  add () {
-  }
-  subtract () {
-  }
-  multiply () {
-  }
-  divide () {
-  }
-  square () {
-  }
-  squareRoot () {
-  }
+    //write your code here
+    constructor (firstValue, secondValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.pi = 3.14159265359;
+        this.addResult;
+        this.subtractResult;
+        this.multiplyResult = 1;
+        this.divideResult;
+        this.squareResult = 1;
+        this.squareRootResult;
+        this.circleResult;
+    }
+
+    add () {            // method menambah
+        this.addResult = this.firstValue + this.secondValue;
+        console.log(this.addResult);
+        return this;
+    }
+
+    subtract () {       // method mengurangi
+        this.subtractResult = this.firstValue - this.secondValue;
+        console.log(this.subtractResult);
+        return this;
+    }
+
+    multiply () {       // method mengali
+        this.multiplyResult = this.firstValue * this.secondValue;
+        console.log(this.multiplyResult);
+        return this;
+    }
+
+    divide () {         // method membagi
+        this.divideResult = this.firstValue / this.secondValue;
+        console.log(this.divideResult);
+        return this;
+    }
+
+    square () {         // pangkat
+        for (let i = 0; i < this.secondValue; i++) {
+            this.squareResult *= this.squareResult;
+        }
+        console.log(this.squareResult);
+        return this;
+    }
+
+    squareRoot () {     // akar pangkat
+        this.squareRoot = Math.sqrt(this.firstValue);
+        console.log(this.squareRoot);
+        return this;
+    }
 }
 
 /** note : you can use several features from ecmascript, such as:
@@ -26,4 +62,4 @@ class Calculator {
 * - Method Chaining
 */
 
-module.exports = Calculator
+module.exports = Calculator;
--- main.js
+++ main.js
@@ -3,3 +3,7 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+
+var calculator = new Calculator(100, 2);
+
+calculator.add().square().divide().squareRoot().multiply().subtract();
