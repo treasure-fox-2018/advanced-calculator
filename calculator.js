8031fe329d81951e659e60acbf9148d7fff58178
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-06-04 15:10:21 +0700

    finish calculator

--- calculator.js
+++ calculator.js
@@ -2,21 +2,47 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (initInt) {
+    this.value = initInt;
+    this.pi = Math.PI;
   }
-  add () {
+  add (addInt) {
+    this.value = this.value + addInt
+    return this;
   }
-  subtract () {
+  subtract (subsInt) {
+    this.value = this.value - subsInt
+    return this;
   }
-  multiply () {
+  multiply (multiplier) {
+    this.value = this.value * multiplier
+    return this;
   }
-  divide () {
+  divide (divider) {
+    this.value = this.value / divider
+    return this;
   }
-  square () {
+  square (power) {
+    this.value = Math.pow(this.value, power)
+    return this;
   }
-  squareRoot () {
+  squareRoot (root) {
+    this.value = Math.pow(this.value, (1/root))
+    return this;
+  }
+  circleCircumference () {
+    this.value = 2 * this.pi * this.value
+    return this;
+  }
+  circleArea () {
+    this.value = this.pi * this.value * this.value
+    return this;
   }
 }
+
+var calculation = new Calculator(4);
+
+console.log(calculation.add(4).circleArea(2).value);
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
