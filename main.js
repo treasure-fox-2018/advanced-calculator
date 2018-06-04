3a88c6b17f094cf6a0bb9746788c327602bbd6d1
Hacktiv8 <hacktiv8@H8JKT002.localdomain>
2018-06-04 15:12:32 +0700

    Brian v 1.0

--- main.js
+++ main.js
@@ -3,3 +3,7 @@
 const Calculator = require("./calculator.js")
 
 // execute function on calculator.js in here
+
+
+let calculate = new Calculator(1)
+console.log(calculate.add(4).multiply(3).subtract(5).num)
